6492bdf3ca658a44c2d64acee364ac9741a08929
uddhavpgautam <infofuniversities@gmail.com>
2023-04-28 20:22:00 -0400

    mvi, jetpack compose, multi module

---
+++ app/src/main/java/com/example/mvi/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mvi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.mvi.ui.theme.MVITheme
+import com.example.steppermodule.presentation.StepperScreen
+import com.example.steppermodule.presentation.StepperView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MVITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    StepperScreen(state = StepperView.State.Error("Loading Error!"))
+                }
+            }
+        }
+    }
+}
---
+++ steppermodule/src/main/java/com/example/steppermodule/presentation/StepperView.kt
@@ -0,0 +1,75 @@
+package com.example.steppermodule.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.steppermodule.R
+
+
+interface StepperView {
+
+    sealed class State {
+        data class Loading(val loadingPercentage: Int): State()
+        object AfterLoading: State()
+        data class Error(val error: String): State()
+        data class Loaded(val newCount: Int): State()
+    }
+
+}
+
+@Composable
+fun StepperScreen(state: StepperView.State) {
+    Column(
+       modifier = Modifier.fillMaxHeight()
+           .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+            StepperRender(state)
+    }
+}
+
+@Composable
+fun StepperRender(state: StepperView.State) {
+    when(state) {
+        is StepperView.State.Loading -> {
+            LoadingState()
+        }
+        is StepperView.State.AfterLoading -> {
+            AfterLoadingState()
+        }
+        is StepperView.State.Error -> {
+            ErrorState(state.error)
+        }
+        is StepperView.State.Loaded -> {
+            LoadedState()
+        }
+    }
+}
+
+@Composable
+fun LoadingState() {
+    CircularProgressIndicator()
+}
+
+@Composable
+fun ErrorState(error: String) {
+    Text("Error: $error")
+}
+
+@Composable
+fun LoadedState() {
+    Text("Loaded")
+}
+
+@Composable
+fun AfterLoadingState() {
+    Text(stringResource(id = R.string.stepperview_string_after_loading_state))
+}
