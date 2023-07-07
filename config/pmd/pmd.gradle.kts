d6bbd5f06526e9b126f6bd6d01d33af767ec26c0
uddhavgautam3 <uddhavgautam3@gmail.com>
2023-07-07 15:08:26 -0400

    pmd.gradle.kts porting done, spotbugs logic moved in module's build.gradle

--- config/pmd/pmd.gradle.kts
+++ config/pmd/pmd.gradle.kts
@@ -3,9 +3,6 @@ apply(from = "${rootDir}/scripts/common.gradle") //for appModuleSourceSets
 
 configure<PmdExtension> {
     toolVersion = "6.55.0"
-
-    ignoreFailures = true // Set to true to continue the build even if PMD violations are found
-
     //ruleSetFiles = files("path/to/ruleset1.xml", "path/to/ruleset2.xml")
     //has priority over ruleSets because ruleSetFiles is our configuration based on our need
     //where ruleSets is from pmd plugin
@@ -16,17 +13,19 @@ configure<PmdExtension> {
 val pmdConfiguration by configurations.creating
 
 tasks.withType<Pmd> {
+    ignoreFailures = false
+
     reports {
-        xml.required = true
-        xml.destination = file("${buildDir.absolutePath}/reports/pmd/pmd.xml")
-        html.required = true
-        html.destination = file("$buildDir/reports/pmd/report.html")
+        xml.required.set(true)
+        html.required.set(true)
+        html.setDestination(file("$buildDir/reports/pmd/pmd.html"))
     }
+
 }
 
 (project.extra["appModuleSourceSets"] as? List<String>)?.forEach { sourceSetName: String ->
     tasks.register("pmd${sourceSetName.replaceFirstChar { it.uppercase() }}", Pmd::class) {
-        group = 'verification'
+        group = "verification"
         description = "Runs pmd task for ${sourceSetName.capitalize()} source set"
 
 
@@ -37,15 +36,4 @@ tasks.withType<Pmd> {
 
         classpath = files()
     }
-}
-
-tasks.register<Pmd>("taskPmd") {
-    group = "Verification"
-    ruleSetFiles = files("${rootDir}/config/pmd/pmdmain.xml")
-    ignoreFailures = false
-    source(fileTree("src") {
-        include("**/*.java", "**/*.kt")
-        exclude("**/R.java", "**BuildConfig.java", "**/gen/**")
-    })
-    classpath = files()
 }
